0330c07a0d2954f100b4c37875538f0f68034a5a
Brano Duris <bduris@gmail.com>
2017-01-17 16:16:26 +0100

    update env variables

--- default_config.py
+++ default_config.py
@@ -3,9 +3,9 @@ import os
 # Default app config
 AWS_REGION = 'us-west-2'
 
-consumer_key = os.environ['tc_twitter_consumer_key'] # 'OsQA2BjCokitlGmPgZ5Vxjygk'
-consumer_secret = os.environ['tc_twitter_consumer_secret'] # 'ZIqZHvYNffPbkSA2zTbj63rVIn7Y7vljtAoGR6c9IjogDnZKgZ'
-access_token_key = os.environ['tc_twitter_access_token_key'] # '702797682335731712-HLOGL4Xjz9SuUzOGCSwaVhvTGeIqSHK'
-access_token_secret = os.environ['tc_twitter_access_token_secret'] # 'mzie5dju8Crd4JFNaPPmcXHrQWgBid5seRrwoxv9dnr76'
+consumer_key = os.environ['tc_twitter_consumer_key']
+consumer_secret = os.environ['tc_twitter_consumer_secret']
+access_token_key = os.environ['tc_twitter_access_token_key']
+access_token_secret = os.environ['tc_twitter_access_token_secret']
 
-SQS_QUEUE_URL = os.environ['sqs_worker_queue_url'] #'https://us-west-2.queue.amazonaws.com/545157427766/awseb-e-8ugv4byjnp-stack-AWSEBWorkerQueue-T6ANROMR84J6'
+SQS_QUEUE_URL = os.environ['sqs_worker_queue_url']
